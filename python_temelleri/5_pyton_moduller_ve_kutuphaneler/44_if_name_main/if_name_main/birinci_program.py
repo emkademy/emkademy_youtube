73740299d003fcc1016b3f145536e902aec1d6e7
Kivanc Yuksel <kivanc.yuksel@yandex.com>
2020-04-27 16:20:08 +0200

    if __name__ == __main__ scriptleri eklendi

---
+++ python_temelleri/5_pyton_moduller_ve_kutuphaneler/44_if_name_main/if_name_main/birinci_program.py
@@ -0,0 +1,17 @@
+# Birinci program
+import ikinci_program
+
+
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+
+
+if __name__ == "__main__":
+   func1() 
+   func2()
+   ikinci_program.ikinci_program_func_3()
---
+++ python_temelleri/5_pyton_moduller_ve_kutuphaneler/44_if_name_main/if_name_main/ikinci_program.py
@@ -0,0 +1,19 @@
+# Ikinci program
+
+
+def ikinci_program_func_1():
+    pass
+
+def ikinci_program_func_2():
+    pass
+
+def ikinci_program_func_3():
+    pass
+
+def ikinci_program_func_4():
+    pass
+
+
+if __name__ == "__main__":
+    print("Merhaba dunya")
+    print("")
